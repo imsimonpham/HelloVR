c3e76e10a9e2d9f7edffefbaeaf8c70ab8ef895e
simon <simonpham1294@gmail.com>
2023-11-21 16:04:12 -0400

    Added teleportation and hands

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(-74f, 1f, -40f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
